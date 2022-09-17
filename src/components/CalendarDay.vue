358e92015c0d703432753f55544e178e03d5f760
KarnesTH <p_haehnel@hotmail.de>
2022-09-17 19:10:20 +0200

    init

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card border-start" :class="cardClasses">
+    <div class="card-header text-center" :class="cardHeaderClasses" role="button">
+      <strong>{{ day.fullName }}</strong>
+    </div>
+    <div class="card-body">
+      <!-- Anfang: Template für die Calendar-Event-Component -->
+      <CalendarEvent v-for="event in day.events" :key="event.title" :event="event">
+        <template #eventPriority="slotProps"><h5>{{ slotProps.priorityDisplayName }}</h5></template>
+        <template #default="{ event }"><i>{{ event.title }}</i></template>
+      </CalendarEvent>
+      <!-- Ende: Template für die Calendar-Event-Component -->
+    </div>
+  </div>
+</template>
+
+<script>
+  import CalendarEvent from './CalendarEvent.vue';
+  import Store from '../store';
+  export default {
+    name: "CalendarDay",
+    components: {
+      CalendarEvent,
+    },
+    props: {
+      day: {
+        type: Object,
+        required: true,
+        default() {
+          return {
+            id: -1,
+            fullName: "Fehlender Wochentag",
+            events: [],
+          };
+        },
+        validator(value) {
+          if (Object.keys(value).includes("id")) {
+            return true;
+          }
+        },
+      },
+    },
+    computed: {
+      cardClasses() {
+        return this.day.id === Store.getters.activeDay().id ? ["border-primary"] : null;
+      },
+      cardHeaderClasses() {
+        return this.day.id === Store.getters.activeDay().id ? ["bg-primary", "text-white"] : null;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CalendarEvent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="calendar-event">
+    <div class="alert text-center" :class="alertColor">
+      <div>
+        <slot name="eventPriority" :priorityDisplayName="priorityDisplayName">
+          <strong>{{ priorityDisplayName }}</strong>
+        </slot>
+      </div>
+      <slot :event="event">
+        <div>{{ event.title }}</div>
+      </slot>
+      <div>
+        <i class="fas fa-edit me-2" role="button"></i>
+        <i class="far fa-trash-alt" role="button"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CalendarEvent",
+    props: {
+      event: Object,
+    },
+    computed: {
+      priorityDisplayName() {
+        switch (this.event.priority) {
+          case 1:
+            return "Niedrig";
+          case 0:
+            return "Mittel";
+          case -1:
+            return "Hoch";
+        }
+        return "Unbekannt";
+      },
+      alertColor() {
+        return `alert-${this.event.color}`;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
